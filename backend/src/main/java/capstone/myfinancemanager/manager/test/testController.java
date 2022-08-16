158916a24118e170a04ea03b9295267da07bb6d0
asrarbutt <asrarbutt1986@gmail.com>
2022-08-16 12:37:39 +0200

    add github workflow and frontend to the project

---
+++ backend/src/main/java/capstone/myfinancemanager/manager/test/testController.java
@@ -0,0 +1,13 @@
+package capstone.myfinancemanager.manager.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping(path = "hallo")
+    public String getHallo(){
+        return "hallo";
+    }
+}
