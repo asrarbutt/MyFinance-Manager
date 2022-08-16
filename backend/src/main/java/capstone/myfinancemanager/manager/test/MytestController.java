47db6d96aeef034a224add54d4f11fce1e079ed3
asrarbutt <asrarbutt1986@gmail.com>
2022-08-16 15:21:45 +0200

    change testController and rename it

--- backend/src/main/java/capstone/myfinancemanager/manager/test/MytestController.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/test/MytestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testController {
+public class MytestController {
 
     @GetMapping(path = "hallo")
     public String getHallo(){
