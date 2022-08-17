86001d3543db8922f615faa4c226dedcb08d4213
asrarbutt <asrarbutt1986@gmail.com>
2022-08-17 17:33:12 +0200

    add Photo class

---
+++ backend/src/main/java/capstone/myfinancemanager/manager/model/Photo.java
@@ -0,0 +1,31 @@
+package capstone.myfinancemanager.manager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Photo {
+
+    @Id
+    @NotNull
+    @NotEmpty
+    private String id;
+
+    @NotNull
+    @NotEmpty
+    private String title;
+
+    @NotNull
+    @NotEmpty
+    private String image;
+
+}
