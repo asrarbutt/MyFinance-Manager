7ae4164d64299c3e5c4dfeb65f7329f5eac18b18
asrarbutt <asrarbutt1986@gmail.com>
2022-08-17 17:08:11 +0200

    add javax validation for the fields

--- backend/src/main/java/capstone/myfinancemanager/manager/model/User.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/model/User.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.time.Instant;
 
 @Data
@@ -15,9 +18,14 @@ import java.time.Instant;
 public class User {
 
     @Id
+    @NotNull(message = "Name is mandatory")
     String name;
+    @Email(message = "Email is mandatory")
+    @NotNull
     String email;
+    @NotNull(message = "Password is mandatory")
     String password;
+    @NotNull(message = "Password is mandatory")
     Instant userRegistrationTime;
 
 }
