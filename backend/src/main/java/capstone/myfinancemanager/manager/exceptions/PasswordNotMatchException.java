83bb8c0d75264f82c43d6b798c09637b00330ec4
asrarbutt <asrarbutt1986@gmail.com>
2022-08-19 14:45:31 +0200

    Change ExceptionType

--- backend/src/main/java/capstone/myfinancemanager/manager/exceptions/PasswordNotMatchException.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/exceptions/PasswordNotMatchException.java
@@ -1,10 +1,7 @@
 package capstone.myfinancemanager.manager.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-public class PasswordNotMatchException extends ResponseStatusException {
-    public PasswordNotMatchException(HttpStatus status, String reason) {
-        super(status, reason);
+public class PasswordNotMatchException extends IllegalStateException {
+    public PasswordNotMatchException(String s) {
+        super(s);
     }
 }
--- backend/src/main/java/capstone/myfinancemanager/manager/exceptions/UserExistsException.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/exceptions/UserExistsException.java
@@ -1,11 +1,8 @@
 package capstone.myfinancemanager.manager.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+public class UserExistsException extends IllegalStateException {
 
-public class UserExistsException extends ResponseStatusException {
-
-    public UserExistsException(HttpStatus status, String reason) {
-        super(status, reason);
+    public UserExistsException(String s) {
+        super(s);
     }
 }
