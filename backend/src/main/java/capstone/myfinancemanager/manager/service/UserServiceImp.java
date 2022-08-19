7a3a8f1c7e6c408cbfe4554a948bcc9e06941d48
asrarbutt <asrarbutt1986@gmail.com>
2022-08-19 14:46:55 +0200

    change the addNewUser math, don´t save the repeatPassword in db

--- backend/src/main/java/capstone/myfinancemanager/manager/service/UserServiceImp.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/service/UserServiceImp.java
@@ -4,7 +4,6 @@ import capstone.myfinancemanager.manager.exceptions.PasswordNotMatchException;
 import capstone.myfinancemanager.manager.exceptions.UserExistsException;
 import capstone.myfinancemanager.manager.model.User;
 import capstone.myfinancemanager.manager.respository.UserRepo;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -23,20 +22,19 @@ public class UserServiceImp {
     public User registerNewUser(User user) {
 
         if (userRepo.findById(user.getEmail()).isPresent()) {
-            throw new UserExistsException(HttpStatus.BAD_REQUEST, "User Exists. Please choose another E-Mail");
+            throw new UserExistsException("User Exists. Please choose another E-Mail");
         } else if (!user.getPassword().equals(user.getRepeatPassword())) {
-            throw new PasswordNotMatchException(HttpStatus.BAD_REQUEST, "Passwords do not match");
+            throw new PasswordNotMatchException("Passwords do not match");
 
         }
-        return userRepo.save(addNewUser(user.getEmail(), user.getName(), user.getPassword(), user.getRepeatPassword()));
+        return userRepo.save(addNewUser(user.getEmail(), user.getName(), user.getPassword()));
     }
 
-    public User addNewUser(String email, String name, String password, String repeatPassword) {
+    public User addNewUser(String email, String name, String password) {
         return User.builder()
                 .email(email)
                 .name(name)
                 .password(password)
-                .repeatPassword(repeatPassword)
                 .userRegistrationDate(Instant.now())
                 .build();
     }
