d6fab6a8028872281440ceba0dd3dd633e5abeb7
asrarbutt <asrarbutt1986@gmail.com>
2022-08-22 10:01:28 +0200

    add components

---
+++ frontend/src/components/AllRoutes.tsx
@@ -0,0 +1,20 @@
+import {Route, Routes} from "react-router-dom";
+import RegistrationsPage from "../pages/RegistrationsPage";
+import LoginPage from "../pages/LoginPage";
+import Home from "../pages/Home";
+
+export default function AllRoutes() {
+
+
+    return (
+        <>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/auth/login"} element={<LoginPage/>}/>
+                <Route path={"/auth/Signup"} element={<RegistrationsPage/>}/>
+            </Routes>
+
+        </>
+    )
+
+}
