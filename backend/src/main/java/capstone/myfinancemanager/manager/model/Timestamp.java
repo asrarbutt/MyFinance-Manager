a0dcdb40c752222b53edc3af182429d201d38616
asrarbutt <asrarbutt1986@gmail.com>
2022-08-23 12:28:50 +0200

    add Annotation

--- backend/src/main/java/capstone/myfinancemanager/manager/model/Timestamp.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/model/Timestamp.java
@@ -1,9 +1,11 @@
 package capstone.myfinancemanager.manager.model;
 
+import org.springframework.stereotype.Service;
+
 import java.time.Instant;
 
+@Service
 public class Timestamp {
-
     public Instant now() {
         return Instant.now();
     }
