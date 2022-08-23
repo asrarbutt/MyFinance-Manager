0c317332570d57625f3669b10f22856cbc4ac5c2
asrarbutt <asrarbutt1986@gmail.com>
2022-08-23 10:40:08 +0200

    add Header in app.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,10 @@
-import Header from "../components/Header";
-
 export default function Home() {
 
 
     return (
         <>
-            <Header/>
-            <h1>Home Page</h1>
+
+        <h1>Home Page</h1>
 
         </>
     )
