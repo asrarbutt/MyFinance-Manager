0b47d519bf96e3ea04e099731f58e25504a3e820
asrarbutt <asrarbutt1986@gmail.com>
2022-08-23 17:28:25 +0200

    add class level ResponseStatus

--- backend/src/main/java/capstone/myfinancemanager/manager/exceptions/PasswordNotMatchException.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/exceptions/PasswordNotMatchException.java
@@ -1,8 +1,11 @@
 package capstone.myfinancemanager.manager.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class PasswordNotMatchException extends RuntimeException {
     public PasswordNotMatchException(String exceptionMessage) {
-
 
         super(exceptionMessage, null, false, false);
     }
