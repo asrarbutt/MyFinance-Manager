4e16b6b4ea8c2d3d4d558e24cc412bac9ceda811
asrarbutt <asrarbutt1986@gmail.com>
2022-08-24 21:40:35 +0200

    change export name

--- frontend/src/context/authentication/AuthContext.ts
+++ frontend/src/context/authentication/AuthContext.ts
@@ -3,8 +3,8 @@ import {createContext} from "react";
 
 export interface IContext {
     register: (email: string, name: string, password: string, repeatPassword: string) => Promise<Response>;
-    login: (email: string, password: string) => Promise<void>;
-    logout: () => void;
 }
 
-export default createContext({} as IContext);
+export const AutoContext = createContext({} as IContext);
+
+export default AutoContext;
