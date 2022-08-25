e3a257cd20556d86fff529b2beee19707b535cbb
asrarbutt <asrarbutt1986@gmail.com>
2022-08-25 10:48:26 +0200

    add custom style

--- frontend/src/components/MyMenu.tsx
+++ frontend/src/components/MyMenu.tsx
@@ -1,26 +1,32 @@
 import {Box, Button, Container} from "@mui/material";
+import {styled} from "@mui/system";
 
-
+const StyledButton = styled(Button)`
+  background: linear-gradient(45deg, #3B9AE1 50%, #FF8E53 95%);
+  border-radius: 2px;
+  border: 0;
+  border-radius:230px;
+  color: white;
+  height: 5rem;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .30);
+  margin:3px
+   
+`;
 export default function MyMenu() {
 
     return (
         <Box sx={{mt: 1, padding: '1rem'}}>
             <Container>
                 <div>
-
-                    <Button>Statistik</Button>
-                    <Button>Transaktion Hinzufügen</Button>
-                    <Button>Einkommen</Button>
-                    <Button>Ausgaben</Button>
-
+                    <StyledButton>Statistik</StyledButton>
+                    <StyledButton>Transaktion Hinzufügen</StyledButton>
+                    <StyledButton>Einkommen</StyledButton>
+                    <StyledButton>Ausgaben</StyledButton>
                 </div>
-
             </Container>
-
-
         </Box>
     )
 }
-
 
 
