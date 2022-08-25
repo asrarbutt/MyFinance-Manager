e5cb6daefd67298f0104118f91b3b9f937aee948
asrarbutt <asrarbutt1986@gmail.com>
2022-08-25 10:09:02 +0200

    remove comments

---
+++ frontend/src/context/authentication/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import UserRegisterData from "../../model/UserRegisterData";
+import axios from "axios";
+import AuthContext from "./AuthContext";
+
+interface Param {
+    children?: any;
+}
+
+export default function AuthProvider({children}: Param) {
+
+    const register = (email: string, name: string, password: string, repeatPassword: string) => {
+
+        const newUser: UserRegisterData = {
+            "email": email,
+            "name": name,
+            "password": password,
+            "repeatPassword": repeatPassword
+        }
+
+        return axios.post("/auth/register", newUser)
+            .then((response) => response.data)
+
+    }
+
+    return (
+
+        <AuthContext.Provider value={{register}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
