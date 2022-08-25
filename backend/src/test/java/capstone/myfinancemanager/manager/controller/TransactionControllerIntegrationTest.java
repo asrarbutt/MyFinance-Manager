a98357f613e2f777eb15916840e7095030c0b922
asrarbutt <asrarbutt1986@gmail.com>
2022-08-25 12:35:09 +0200

    add transactionController with getAllTransaction method

---
+++ backend/src/test/java/capstone/myfinancemanager/manager/controller/TransactionControllerIntegrationTest.java
@@ -0,0 +1,37 @@
+package capstone.myfinancemanager.manager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TransactionControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    void getAllTransactions() throws Exception {
+
+        mockMvc
+                .perform(
+                        MockMvcRequestBuilders.get("/transaction")
+                )
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$", hasSize(0)));
+
+    }
+
+
+}
