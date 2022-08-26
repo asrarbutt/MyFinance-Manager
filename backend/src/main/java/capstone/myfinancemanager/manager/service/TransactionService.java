ed218a0c591a5009b43d980c32d8032d037c79da
asrarbutt <asrarbutt1986@gmail.com>
2022-08-26 17:42:47 +0200

    add function for adding addTransaction

--- backend/src/main/java/capstone/myfinancemanager/manager/service/TransactionService.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/service/TransactionService.java
@@ -1,19 +1,21 @@
 package capstone.myfinancemanager.manager.service;
 
 
+import capstone.myfinancemanager.manager.model.RandomUUIDGenerator;
+import capstone.myfinancemanager.manager.model.Transaction;
 import capstone.myfinancemanager.manager.model.dto.TransactionDto;
 import capstone.myfinancemanager.manager.respository.TransactionRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
-
-
 @RequiredArgsConstructor
 @Service
 public class TransactionService {
 
     private final TransactionRepo transactionRepo;
+    private final RandomUUIDGenerator randomUUIDGenerator;
 
     public List<TransactionDto> getAllTransactions() {
 
@@ -29,5 +31,20 @@ public class TransactionService {
                                 t.isIncome(),
                                 t.getPictureId()))
                 .toList();
+    }
+
+    public Transaction addTransaction(TransactionDto newTransactionDto) {
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setId(randomUUIDGenerator.getRandomId());
+        newTransaction.setDescription(newTransactionDto.getDescription());
+        newTransaction.setAmount(newTransactionDto.getAmount());
+        newTransaction.setTransactionDate(Instant.now());
+        newTransaction.setCategory(newTransactionDto.getCategory());
+        newTransaction.setPictureId(newTransactionDto.getPictureId());
+        newTransaction.setIncome(newTransactionDto.isIncome());
+        newTransaction.setUserEmail("kommt aus dem login");
+
+        return transactionRepo.save(newTransaction);
     }
 }
