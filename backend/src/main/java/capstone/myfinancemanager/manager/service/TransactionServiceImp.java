3f4bcb1deb5d4143b387ac575efec64986d8e9d2
asrarbutt <asrarbutt1986@gmail.com>
2022-08-26 15:13:45 +0200

    update TransactionServiceImp -change return typ of getAllTransactions

--- backend/src/main/java/capstone/myfinancemanager/manager/service/TransactionServiceImp.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/service/TransactionServiceImp.java
@@ -1,11 +1,13 @@
 package capstone.myfinancemanager.manager.service;
 
-import capstone.myfinancemanager.manager.model.Transaction;
+
+import capstone.myfinancemanager.manager.model.dto.TransactionDto;
 import capstone.myfinancemanager.manager.respository.TransactionRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @AllArgsConstructor
 @Service
@@ -13,8 +15,19 @@ public class TransactionServiceImp {
 
     private final TransactionRepo transactionRepo;
 
-    public List<Transaction> getAllTransactions() {
+    public List<TransactionDto> getAllTransactions() {
 
-        return transactionRepo.findAll();
+
+        return transactionRepo.findAll()
+                .stream()
+                .map(t ->
+                        new TransactionDto(
+                                t.getDescription(),
+                                t.getAmount(),
+                                t.getTransactionDate(),
+                                t.getCategory(),
+                                t.isIncome(),
+                                t.getPictureId()))
+                .toList();
     }
 }
