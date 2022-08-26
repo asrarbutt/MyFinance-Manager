1114fc1a1d31bdcd476ee06e14d010f70b3d90f5
asrarbutt <asrarbutt1986@gmail.com>
2022-08-26 11:19:49 +0200

    update MyCard.tsx change the style of the card

--- frontend/src/components/MyCard.tsx
+++ frontend/src/components/MyCard.tsx
@@ -1,18 +1,59 @@
 import * as React from 'react';
 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
+import {IconButton, Typography} from "@mui/material";
+import {Box, styled} from "@mui/system";
+import TransactionData from "../model/TransactionData";
 
 
-export default function MyCard() {
+const StlyeDeleteIcon = styled(DeleteForeverIcon)`
+
+  font-size: 2rem;
+ `;
+
+const StlyeEditIcon = styled(EditIcon)`
+
+  font-size: 2rem;
+ `;
+
+
+type MyCardProps = {
+
+    allTransaction: TransactionData;
+
+}
+
+
+export default function MyCard(props: MyCardProps) {
+
+
     return (
-        <Card sx={{width: 700, minWidth: 245, ml: 5, mr: 5}}>
-            <CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
+        <Card sx={{width: 900, height: '20', minWidth: 245, ml: 5, mr: 5, borderRadius: '15px'}}>
+            <CardContent sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',}}>
+
+                <Typography sx={{fontSize: 14}} color="text.secondary">
+                    {props.allTransaction.description}
+                </Typography>
+
+                <Typography sx={{fontSize: 14}} color="text.secondary">
+                    {props.allTransaction.amount}
+                </Typography>
+
+                <Typography sx={{fontSize: 14}} color="text.secondary">
+                    {props.allTransaction.category}
+                </Typography>
+
+                <Box>
+                    <IconButton>
+                        <StlyeDeleteIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <StlyeEditIcon/>
+                    </IconButton>
+                </Box>
             </CardContent>
 
         </Card>
