a384732ae0bbdaad31e1882e61911e81533c82a1
asrarbutt <asrarbutt1986@gmail.com>
2022-08-29 15:02:22 +0200

    update Icontext.tsx - add login, logout, userLoggedIn

--- frontend/src/context/authentication/AuthContext.ts
+++ frontend/src/context/authentication/AuthContext.ts
@@ -3,6 +3,10 @@ import {createContext} from "react";
 
 export interface IContext {
     register: (email: string, name: string, password: string, repeatPassword: string) => Promise<Response>;
+    login: (username: string, password: string) => Promise<void>;
+    userLoggedIn: string;
+    logout: () => void;
+
 }
 
 export const AuthContext = createContext({} as IContext);
