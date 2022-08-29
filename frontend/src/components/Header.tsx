d31918423979a3b4ad5f658ea7da6fe539f7da8a
asrarbutt <asrarbutt1986@gmail.com>
2022-08-29 15:01:08 +0200

    update Header - add props

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import MyNavBar from "./MyNavBar";
+import {useContext} from "react";
+import AuthContext from "../context/authentication/AuthContext";
 
 export default function Header() {
+    const {userLoggedIn, logout} = useContext(AuthContext);
+
 
     return (
 
         <header>
-            <MyNavBar/>
+            <MyNavBar loggedInUser={userLoggedIn} logout={logout}/>
         </header>
     )
 
