46e3005ad4d0221318bd3732282a984bdb3ec283
asrarbutt <asrarbutt1986@gmail.com>
2022-08-30 23:07:39 +0200

    update TransactionServiceTest fix issue

--- backend/src/test/java/capstone/myfinancemanager/manager/service/TransactionServiceTest.java
+++ backend/src/test/java/capstone/myfinancemanager/manager/service/TransactionServiceTest.java
@@ -26,45 +26,73 @@ class TransactionServiceTest {
 
     private final Instant testDate = Instant.parse("2022-08-23T09:22:41.255023Z");
     private final String randomTestId = "1";
-    private final Transaction transaction =
-            new Transaction(
-                    randomTestId,
-                    "Essen", 25.0,
-                    testDate, "TestCategory",
-                    "testmail@test.com",
-                    false,
-                    "url");
-
-    private final TransactionDto transactionDto =
-            new TransactionDto(
-                    "Essen",
-                    25.0,
-                    testDate,
-                    "TestCategory",
-                    false,
-                    "url");
 
 
-    private final List<Transaction> testTransactions = List.of(
+    Transaction transaction1 = Transaction.builder().id(randomTestId)
+            .description("Essen")
+            .amount(25.0)
+            .transactionDate(testDate)
+            .category("TestCategory")
+            .userEmail("test@test.com")
+            .isIncome(false)
+            .pictureId("url").build();
+
+
+    Transaction transaction2 = Transaction.builder().id(randomTestId)
+            .description("Tanken")
+            .amount(27.0)
+            .transactionDate(testDate)
+            .category("TestCategory")
+            .userEmail("test@test.com")
+            .isIncome(true)
+            .pictureId("url").build();
+
+    TransactionDto transactionDto1 = TransactionDto.builder()
+            .userEmail("test@test.com")
+            .description("Tanken")
+            .amount(27.0)
+            .transactionDate(testDate)
+            .category("TestCategory")
+            .isIncome(true)
+            .pictureId("url")
+            .build();
+
+    TransactionDto transactionDto2 = TransactionDto.builder()
+            .userEmail("test@test.com")
+            .description("Essen")
+            .amount(25.0)
+            .transactionDate(testDate)
+            .category("TestCategory")
+            .isIncome(false)
+            .pictureId("url")
+            .build();
+
+
+
+
+/*    private final List<Transaction> testTransactions = List.of(
             new Transaction(randomTestId, "Essen", 25.0, testDate, "TestCategory", "testmail@test.com", false, "url"),
             new Transaction(randomTestId, "Tanken", 25.0, testDate, "TestCategory", "testmail@test.com", false, "url")
     );
 
+
     private final List<TransactionDto> testTransactionsDto = List.of(
-            new TransactionDto("Essen", 25.0, testDate, "TestCategory", false, "url"),
-            new TransactionDto("Tanken", 25.0, testDate, "TestCategory", false, "url")
-    );
+            new TransactionDto("testmail@test.com", "Essen", 25.0, testDate, "TestCategory", false, "url"),
+            new TransactionDto("testmail@test.com", "Tanken", 25.0, testDate, "TestCategory", false, "url")
+    );*/
 
     @Test
     void getAllTransactionsTest() {
 
         //when
+        when(randomUUIDGenerator.getRandomId()).thenReturn(randomTestId);
         when(timestampService.now()).thenReturn(Instant.parse("2022-08-23T09:22:41.255023Z"));
-        when(transactionRepo.findAll()).thenReturn(testTransactions);
+        when(transactionRepo.findAll()).thenReturn(List.of(transaction1, transaction2));
         List<TransactionDto> actual = transactionService.getAllTransactions();
 
+
         //then
-        Assertions.assertArrayEquals(testTransactionsDto.toArray(), actual.toArray());
+        Assertions.assertEquals(actual.get(0).getUserEmail(), transaction2.getUserEmail());
     }
 
     @Test
@@ -73,10 +101,10 @@ class TransactionServiceTest {
         //when
         when(randomUUIDGenerator.getRandomId()).thenReturn(randomTestId);
         when(timestampService.now()).thenReturn(Instant.parse("2022-08-23T09:22:41.255023Z"));
-        when(transactionRepo.save(any())).thenReturn(transaction);
+        when(transactionRepo.save(any())).thenReturn(transaction1);
 
-        Transaction actual = transactionRepo.save(transaction);
-        Assertions.assertEquals(transaction, actual);
+        Transaction actual = transactionRepo.save(transaction1);
+        Assertions.assertEquals(transaction1, actual);
 
     }
 
