d0e38844f31bd23266086cee847f256ee0873a84
asrarbutt <asrarbutt1986@gmail.com>
2022-08-31 12:19:28 +0200

    update util.ts
    change the convertDateNumber return

--- frontend/src/util/Util.ts
+++ frontend/src/util/Util.ts
@@ -2,17 +2,21 @@ export const dateFromInstant = (instant: number | null, locale: string) => {
 
     if (instant !== null) {
         const date = new Date(instant);
+        console.log(instant)
         const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'numeric', day: 'numeric'};
         return date.toLocaleDateString((locale) ? locale : undefined, options)
     }
-
-
 }
 
 export const stringToNumberWithDot = (input: string) => {
     return Number(input.replaceAll(",", "."));
 }
 
-export const convertDateToNumber = (input: Date | null) => {
-    return Number(input);
+export const convertDateToNumber = (date: Date | null) => {
+
+    let dateInstant = null
+    if (!!date && !!date.getTime()) {
+        dateInstant = date.getTime();
+    }
+    return dateInstant;
 }
