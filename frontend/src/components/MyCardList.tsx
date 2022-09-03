8acf04200ce12c39f3624a01db4d9113a856d391
asrarbutt <asrarbutt1986@gmail.com>
2022-09-03 12:43:36 +0200

    update
    -clean the code
    -remove unnecessary code

--- frontend/src/components/MyCardList.tsx
+++ frontend/src/components/MyCardList.tsx
@@ -1,33 +1,18 @@
 import MyCard from "./MyCard";
-import {useContext, useEffect, useState} from "react";
+import {useContext} from "react";
 import TransactionContext from "../context/transaction/TransactionContext";
-import {toast} from "react-toastify";
-import TransactionDto from "../model/TransactionDto";
+
 
 export default function MyCardList() {
 
-    const {getAllTransactions, deleteTransaction} = useContext(TransactionContext);
-
-    const [allTransactions, setAllTransactions] = useState<TransactionDto[]>([]);
-
-    useEffect(() => {
-
-        getAllTransactions()
-            .then(data => {
-                return setAllTransactions(data)
-            })
-            .catch(error => {
-                toast.error(error.message)
-            });
-
-    }, [allTransactions])// eslint-disable-line
+    const {deleteTransaction, allTransaction} = useContext(TransactionContext);
 
     return (
         <>
             {
-                allTransactions.length !== 0 ? (
-                    allTransactions.map(t =>
-                        <MyCard allTransaction={t} deleteTransaction={deleteTransaction}/>
+                allTransaction.length !== 0 ? (
+                    allTransaction.map(t =>
+                        <MyCard key={t.id} allTransaction={t} deleteTransaction={deleteTransaction}/>
                     )
                 ) : (<h1>Keine Transaktion vorhanden</h1>)
             }
