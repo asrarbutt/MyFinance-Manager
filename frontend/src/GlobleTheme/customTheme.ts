710348d87b96412f53f4e518c9c2cde27a223803
asrarbutt <asrarbutt1986@gmail.com>
2022-09-05 09:46:30 +0200

    update customTheme.ts add more variable

--- frontend/src/GlobleTheme/customTheme.ts
+++ frontend/src/GlobleTheme/customTheme.ts
@@ -1,15 +1,48 @@
 import {createTheme} from "@mui/material";
 
+declare module "@mui/material/styles" {
+    interface CustomPalette {
+        neutral: {
+            main: string;
+            contrastText: string;
+        };
+        backgroundColor: string;
+        navbarBackgroundColor: string;
+        linkButtonColor: string;
+        linkButtonFontColor: string;
+    }
+
+    interface Palette extends CustomPalette {
+    }
+
+    interface PaletteOptions extends CustomPalette {
+    }
+}
+
+
 export const myTheme = createTheme({
     palette: {
         primary: {
-            main: '#ea0d74',
+            main: '#112b3c',
         },
         secondary: {
             main: '#9acb79',
-        }
+        },
+
+        neutral: {
+            main: '#fff',
+            contrastText: '#002255',
+        },
+        backgroundColor: '#7f0aec',
+        navbarBackgroundColor: '#0258B4',
+        linkButtonColor: '#f57b29',
+        linkButtonFontColor: '#EFEFEF'
+
     },
 
+    shape: {
+        borderRadius: 5
+    },
     typography: {
         fontFamily: [
             'Nunito',
@@ -17,12 +50,17 @@ export const myTheme = createTheme({
         ].join(','),
         fontWeightRegular: 400,
         fontWeightMedium: 600,
+        fontSize: 13,
         body1: {
             fontWeight: '400',
         },
         allVariants: {
             wordWrap: 'break-word',
         },
-
+        button: {
+            fontStyle: 'bold',
+            fontSize: 15,
+        }
     },
+
 })
