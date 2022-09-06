9a1a65906e2dba748a0d6fd3ce14218c761f292e
asrarbutt <asrarbutt1986@gmail.com>
2022-09-06 11:46:09 +0200

    review fix
    -remove useContext
    - remove props for MyNavbar component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,9 @@
 import MyNavBar from "./MyNavBar";
-import {useContext} from "react";
-import AuthContext from "../context/authentication/AuthContext";
 
 export default function Header() {
-    const {loggedInUser, logout} = useContext(AuthContext);
-
     return (
-
         <header>
-            <MyNavBar loggedInUser={loggedInUser} logout={logout}/>
+            <MyNavBar/>
         </header>
     )
 
