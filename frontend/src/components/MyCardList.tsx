618519fe248071dda3fae5d851dec93866337313
asrarbutt <asrarbutt1986@gmail.com>
2022-09-06 11:51:11 +0200

    review fix
    -change to typo to allTransactions

--- frontend/src/components/MyCardList.tsx
+++ frontend/src/components/MyCardList.tsx
@@ -5,18 +5,18 @@ import TransactionContext from "../context/transaction/TransactionContext";
 
 export default function MyCardList() {
 
-    const {deleteTransaction, allTransaction} = useContext(TransactionContext);
+    const {deleteTransaction, allTransactions} = useContext(TransactionContext);
 
     return (
         <Box>
             <h1>Alle Transaktionen</h1>
 
             {
-                allTransaction.length !== 0 ? (
-                    allTransaction.map(t =>
+                allTransactions.length !== 0 ? (
+                    allTransactions.map(t =>
                         <MyCard key={t.id} allTransaction={t} deleteTransaction={deleteTransaction}/>
                     )
-                ) : (<h1>Keine Transaktion vorhanden</h1>)
+                ) : (<p>Keine Transaktion vorhanden</p>)
             }
 
         </Box>
