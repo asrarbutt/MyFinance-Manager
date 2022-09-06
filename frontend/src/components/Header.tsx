50b123cfeca01720eb9253de424ac613fc967519
asrarbutt <asrarbutt1986@gmail.com>
2022-09-07 00:48:12 +0200

    update Header.tsx - remove header tag

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,9 @@ import MyNavBar from "./MyNavBar";
 
 export default function Header() {
     return (
-        <header>
+        <>
             <MyNavBar/>
-        </header>
+        </>
     )
 
 }
