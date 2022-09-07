d0ffcd37bf0c2b699ba60013017d9f720916f7df
asrarbutt <asrarbutt1986@gmail.com>
2022-09-07 13:11:09 +0200

    add custom Title.styled.ts styled-component

---
+++ frontend/src/components/ui/Title.styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const TitleStyled = styled.h1`
+  color: #212323;
+  font-weight: bold;
+`;
+
+export default TitleStyled;
+
+
+export const WelcomeTitle = styled.h2`
+  margin: 3rem 0 2rem 0;
+`;
+
+
+export const WelcomeText = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 3rem 0 2rem 0;
+`;
