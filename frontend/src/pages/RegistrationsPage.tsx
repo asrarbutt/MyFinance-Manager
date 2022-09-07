f0460c5fbb439dcc76891308468538aecf034e30
asrarbutt <asrarbutt1986@gmail.com>
2022-09-07 17:11:54 +0200

    update - add frontend Validation for userEmail, userPassword

--- frontend/src/pages/RegistrationsPage.tsx
+++ frontend/src/pages/RegistrationsPage.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, TextField} from "@mui/material";
+import {Alert, Box, Button, TextField} from "@mui/material";
 import "./RegistrationsPage.css";
 import {FormEvent, useContext, useState} from "react";
 import {toast} from "react-toastify";
@@ -20,31 +20,73 @@ export default function RegistrationsPage() {
 
         event.preventDefault();
 
-        register(userEmail, userName, userPassword, userRepeatPassword).then(() => {
-            if (userPassword !== userRepeatPassword) {
-                toast.error('Passwords do not match')
-            } else {
-                setUserEmail("");
-                setUserName("");
-                setUserPassword("");
-                setUserRepeatPassword("");
-                setErrorMessage("")
-                toast.success("Account Created!")
-                navigate("/");
-            }
-        })
-            .catch(error => {
-                setErrorMessage(error.response.data.message)
-                toast.error(error.response.data.message);
-                console.log(error.response.data.message)
-            });
+        if (validateEmail(userEmail) && validatePasswords(userPassword, userRepeatPassword)) {
+
+            register(userEmail, userName, userPassword, userRepeatPassword).then(() => {
+                if (userPassword !== userRepeatPassword) {
+                    toast.error('Passwords do not match')
+                } else {
+                    setUserEmail("");
+                    setUserName("");
+                    setUserPassword("");
+                    setUserRepeatPassword("");
+                    setErrorMessage("")
+                    toast.success("Account Created!")
+                    navigate("/auth/login");
+                }
+            })
+                .catch(error => {
+                    setErrorMessage(error.response.data.message)
+                    toast.error(error.response.data.message);
+                    console.log(error.response.data.message)
+                });
+        }
+    }
+
+    const validateEmail = (inputEmail: string) => {
+
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail)) {
+            setErrorMessage("Email ist not valid")
+        }
+        setErrorMessage("");
+        return true;
+    }
+
+    const validatePasswords = (password1: string, repeatPassword: string) => {
+        if (password1 === "" || repeatPassword === "") {
+            setErrorMessage("Password/confirm Password is required")
+            return false
+        }
+
+        if (password1.length < 6) {
+            setErrorMessage("Password to short minimum 6 Characters")
+            return false
+        }
+
+        if (!/[a-z]/.test(password1)) {
+            setErrorMessage("minimum one lowercase Character required")
+            return false
+        }
+
+        if (!/[A-Z]/.test(password1)) {
+            setErrorMessage("minimum one Uppercase Character required")
+            return false
+        }
+
+        if (password1 !== repeatPassword) {
+            setErrorMessage("Passwords do not match")
+            return false
+        }
+
+        setErrorMessage("")
+        return true;
     }
 
     return (
         <Box className="signUp" sx={{boxShadow: "0 0.1rem 0.2rem rgba(0, 0, 0, 0.5)"}}>
             <h1>Registrieren</h1>
             <h3>Es geht schnell, einfach und kostenlos</h3>
-            {errorMessage && <Box>{errorMessage}</Box>}
+            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
 
             <form onSubmit={handleSubmit} className="signUp-form">
                 <TextField required label="E-Mail" type="email" variant="filled" value={userEmail}
@@ -58,7 +100,6 @@ export default function RegistrationsPage() {
                            onChange={(e) => setUserRepeatPassword(e.target.value)}/>
                 <Button type="submit" variant="outlined">Register</Button>
                 <Link to="/auth/login">Back to login</Link>
-
             </form>
         </Box>
     )
