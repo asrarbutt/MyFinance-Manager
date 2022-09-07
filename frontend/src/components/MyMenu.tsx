2ab08792f9cc2b04a46c26388f69ab57845f73a4
asrarbutt <asrarbutt1986@gmail.com>
2022-09-07 12:54:43 +0200

    update MyMenu.tsx - remove empty lines - clean code - add now custom styled buttons

--- frontend/src/components/MyMenu.tsx
+++ frontend/src/components/MyMenu.tsx
@@ -1,56 +1,17 @@
-import {Box, Button} from "@mui/material";
-import {styled} from "@mui/system";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AddTransaction from "./AddTransaction";
-import {useTheme} from "@mui/material/styles";
-
-
-const StyleAddCircleIcon = styled(AddCircleIcon)`
-font-size:5rem;
-`;
-
-const StyledButton = styled(Button)`
-  background: linear-gradient(45deg, #3B9AE1 70%, #FF8E53 99%);
-  border-radius: 2px;
-  border: 0;
-  border-radius:230px;
-  color: white;
-  height: 5rem;
-  padding: 0 30px;
-  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .30);
-  margin:3px;
-  font-size: 1rem;
- `;
+import {MenuButton} from "./ui/Button.styled";
 
 export default function MyMenu() {
-    const theme = useTheme();
-
-
 
     return (
-        <Box maxWidth={"xs"} sx={{
-            margin: 1,
-            padding: '1rem',
-            display: 'flex',
-            justifyContent: "space-between"
-
-
-        }}>
-            <Box>
-                <Button sx={{background: theme.palette.linkButtonColor, color: theme.palette.linkButtonFontColor}}
-                        variant="contained">Transaktion</Button>
-                <Button sx={{background: theme.palette.linkButtonColor, color: theme.palette.linkButtonFontColor}}
-                        variant="contained">Statistik</Button>
-                <Button sx={{background: theme.palette.linkButtonColor, color: theme.palette.linkButtonFontColor}}
-                        variant="contained">Einkommen</Button>
-                <Button sx={{background: theme.palette.linkButtonColor, color: theme.palette.linkButtonFontColor}}
-                        variant="contained">Ausgaben</Button>
-            </Box>
-
-
+        <div>
+            <div>
+                <MenuButton variant={"myMenuBtn"}>Transaktion</MenuButton>
+                <MenuButton variant={"myMenuBtn"}>Statistik</MenuButton>
+                <MenuButton variant={"myMenuBtn"}>Einkommen</MenuButton>
+                <MenuButton variant={"myMenuBtn"}>Ausgaben</MenuButton>
+            </div>
             <AddTransaction/>
-
-        </Box>
-
+        </div>
     )
 }
