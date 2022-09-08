bf91d17d3c187f00535854c5447d7f3ed0658e2e
asrarbutt <asrarbutt1986@gmail.com>
2022-09-08 13:55:36 +0200

    add navigate for route

--- frontend/src/components/MyMenu.tsx
+++ frontend/src/components/MyMenu.tsx
@@ -1,13 +1,15 @@
 import AddTransaction from "./AddTransaction";
 import {MenuButton} from "./ui/Button.styled";
+import {useNavigate} from "react-router-dom";
 
 export default function MyMenu() {
+    const navigate = useNavigate();
 
     return (
         <div>
             <div>
                 <MenuButton variant={"myMenuBtn"}>Transaktion</MenuButton>
-                <MenuButton variant={"myMenuBtn"}>Statistik</MenuButton>
+                <MenuButton variant={"myMenuBtn"} onClick={() => navigate("/users/statistic")}>Statistik</MenuButton>
                 <MenuButton variant={"myMenuBtn"}>Einkommen</MenuButton>
                 <MenuButton variant={"myMenuBtn"}>Ausgaben</MenuButton>
             </div>
