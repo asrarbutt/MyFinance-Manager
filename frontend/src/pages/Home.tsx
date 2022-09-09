3ff5444d0a67da76b66441f1cefc6ef05c655337
asrarbutt <asrarbutt1986@gmail.com>
2022-09-09 12:06:29 +0200

    add ShowIncomeExpanse component add Props for ShowIncomeExpanse

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import MyMenu from "../components/MyMenu";
 import MyCardList from "../components/MyCardList";
 import {HomeContainerStyled} from "../components/ui/CommonContainer.styled";
+import ShowIncomeExpanse from "../components/ShowIncomeExpanse";
 
+type HomeProsp = {
+    sumOfIncome: number,
+    sumOfExpanse: number
+}
 
-export default function Home() {
+export default function Home(props: HomeProsp) {
 
     return (
         <HomeContainerStyled>
             <MyMenu/>
+            <ShowIncomeExpanse
+                sumOfIncome={props.sumOfIncome}
+                sumOfExpanse={props.sumOfExpanse}
+            />
             <MyCardList/>
         </HomeContainerStyled>
     )
