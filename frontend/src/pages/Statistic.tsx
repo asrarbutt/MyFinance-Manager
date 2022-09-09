1d581d818f30d61b20dc029a746de15ca5ae37b5
asrarbutt <asrarbutt1986@gmail.com>
2022-09-09 12:08:11 +0200

    remove method and add props

--- frontend/src/pages/Statistic.tsx
+++ frontend/src/pages/Statistic.tsx
@@ -2,39 +2,20 @@ import PieChart from "../components/PieChart";
 import "./Statistic.css"
 import {useContext} from "react";
 import TransactionContext from "../context/transaction/TransactionContext";
+import ShowIncomeExpanse from "../components/ShowIncomeExpanse";
 
-export default function Statistic() {
+type StatisticProps = {
+    amounts: number[],
+    groupedCategory: string[],
+    sumOfExpanse: number,
+    sumOfIncome: number,
+    sumOfIncomeAndExpanse: number[]
+}
 
-    const {allTransactions} = useContext(TransactionContext);
+export default function Statistic(props: StatisticProps) {
+
     const transactionstypeCategory = ["Einkommen", "Ausgabe"]
-
-    const grouped = Array.from(
-        allTransactions
-            .reduce(
-                (m, {category, amount}) =>
-                    m.set(category, (m.get(category) || 0) + amount), new Map()),
-        ([key, val]) => ({key, val})
-    );
-
-    const amounts = grouped
-        .filter(t => t.val)
-        .map(t => t.val);
-
-    const groupedCategory = grouped
-        .filter(t => t.key)
-        .map(t => t.key);
-
-    const sumOfExpanse = allTransactions
-        .filter(t => (!t.isIncome))
-        .map(t => t.amount)
-        .reduce((a, b) => a + b, 0);
-
-    const sumOfIncome = allTransactions
-        .filter(t => t.isIncome)
-        .map(t => t.amount)
-        .reduce((a, b) => a + b, 0);
-
-    const sumOfIncomeAndExpanse = Array.of(sumOfIncome, sumOfExpanse);
+    const {allTransactions} = useContext(TransactionContext);
 
     return (
         <div className="statistic">
@@ -44,35 +25,39 @@ export default function Statistic() {
 
             <section className="statistic-section">
                 <div className="statistic-incomeExpanse">
-                    <div className="statistic-showIncome">
-                        <p>Einkommen</p>
-                        <p>{sumOfIncome} €</p>
-                    </div>
-                    <div className="statistic-showExpanse">
-                        <p>Ausgaben</p>
-                        <p>{sumOfExpanse} €</p>
-                    </div>
+
+                    <ShowIncomeExpanse
+                        sumOfIncome={props.sumOfIncome}
+                        sumOfExpanse={props.sumOfExpanse}
+                    />
                 </div>
 
                 <div className="statistic-charts">
                     <div>
-
-                        {amounts.length !== 0 ? (
+                        {props.amounts.length !== 0 ? (
                                 <>
                                     <p>Ausgaben nach Kategorien</p>
-                                    <PieChart allTransactions={allTransactions} transactionsType={groupedCategory}
-                                              amounts={amounts}/>
+
+                                    <PieChart
+                                        allTransactions={allTransactions}
+                                        transactionsType={props.groupedCategory}
+                                        amounts={props.amounts}
+                                    />
                                 </>
                             )
                             : <p>Keine Transaktionen vorhanden</p>
                         }
                     </div>
                     <div>
-                        {amounts.length !== 0 ? (
+                        {props.amounts.length !== 0 ? (
                                 <>
                                     <p>Einkommen und Ausgaben</p>
-                                    <PieChart allTransactions={allTransactions} transactionsType={transactionstypeCategory}
-                                              amounts={sumOfIncomeAndExpanse}/>
+
+                                    <PieChart
+                                        allTransactions={allTransactions}
+                                        transactionsType={transactionstypeCategory}
+                                        amounts={props.sumOfIncomeAndExpanse}
+                                    />
                                 </>
                             )
                             : <p>Kein Transaktionen vorhanden</p>
