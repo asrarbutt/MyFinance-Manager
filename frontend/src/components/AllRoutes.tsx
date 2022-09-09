2c9827aacd866b4621f2509bc6b28ba304c5dc98
asrarbutt <asrarbutt1986@gmail.com>
2022-09-09 12:11:06 +0200

    import useStatistic add props for elements

--- frontend/src/components/AllRoutes.tsx
+++ frontend/src/components/AllRoutes.tsx
@@ -4,20 +4,43 @@ import LoginPage from "../pages/LoginPage";
 import Home from "../pages/Home";
 import LandingPage from "../pages/LandingPage";
 import Statistic from "../pages/Statistic";
+import useStatistic from "../hooks/useStatistic";
 
 export default function AllRoutes() {
 
+    const serviceHook = useStatistic();
 
     return (
         <>
             <Routes>
-                <Route path={"/"} element={<LandingPage/>}/>
-                <Route path={"/home"} element={<Home/>}/>
-                <Route path={"/users/statistic"} element={<Statistic/>}/>
-                <Route path={"/auth/login"} element={<LoginPage/>}/>
-                <Route path={"/auth/register"} element={<RegistrationsPage/>}/>
+                <Route
+                    path={"/"}
+                    element={<LandingPage/>}
+                />
+                <Route
+                    path={"/home"}
+                    element={<Home
+                        sumOfExpanse={serviceHook.sumOfExpanse}
+                        sumOfIncome={serviceHook.sumOfIncome}/>}
+                />
+                <Route
+                    path={"/users/statistic"}
+                    element={<Statistic
+                        amounts={serviceHook.amounts}
+                        groupedCategory={serviceHook.groupedCategory}
+                        sumOfExpanse={serviceHook.sumOfExpanse}
+                        sumOfIncome={serviceHook.sumOfIncome}
+                        sumOfIncomeAndExpanse={serviceHook.sumOfIncomeAndExpanse}/>}
+                />
+                <Route
+                    path={"/auth/login"}
+                    element={<LoginPage/>}
+                />
+                <Route
+                    path={"/auth/register"}
+                    element={<RegistrationsPage/>}
+                />
             </Routes>
         </>
     )
-
 }
