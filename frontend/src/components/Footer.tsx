ceffbef616a80d4e788e5e6462f15801008ad296
asrarbutt <asrarbutt1986@gmail.com>
2022-09-12 08:58:28 +0200

    add footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,19 @@
+import "./Footer.css"
+
 export default function Footer() {
 
     return (
         <footer>
+            <div className="footer-content">
+                <p>copyright & copy;2022 <a href="https://github.com/asrarbutt" target="_blank" rel={"noreferrer"}>Asrar
+                    Ahmad Butt</a></p>
+                <ul className="footer-home">
+                    <li><a href="https://myfinance-manager.herokuapp.com/" target="_blank" rel={"noreferrer"}><i
+                        className="HomeBottom"></i></a></li>
 
-            <p>Footer</p>
+                </ul>
 
+            </div>
         </footer>
     )
 }
