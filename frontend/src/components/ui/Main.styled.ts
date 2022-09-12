57d91a18f37280ba26e91bfabb03a608a64e23d4
asrarbutt <asrarbutt1986@gmail.com>
2022-09-12 08:54:58 +0200

    change style

--- frontend/src/components/ui/Main.styled.ts
+++ frontend/src/components/ui/Main.styled.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const MainStyled = styled.main`
-    text-align: center;
-    margin: 0.5rem 0.5rem;
-    display: flex;
-    justify-content: center;
+  text-align: center;
+  margin: 3.5rem 0.5rem;
+  display: flex;
+  justify-content: center;
 `;
 
 export default MainStyled;
