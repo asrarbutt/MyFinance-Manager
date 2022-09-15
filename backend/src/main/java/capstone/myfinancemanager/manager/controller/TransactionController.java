a21e7da12de341293f61d0195e2ccf94e3bcd453
asrarbutt <asrarbutt1986@gmail.com>
2022-09-15 11:19:04 +0200

    add cloudinary method in controller

--- backend/src/main/java/capstone/myfinancemanager/manager/controller/TransactionController.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/controller/TransactionController.java
@@ -3,16 +3,21 @@ package capstone.myfinancemanager.manager.controller;
 import capstone.myfinancemanager.manager.model.Transaction;
 import capstone.myfinancemanager.manager.model.dto.TransactionCreationDto;
 import capstone.myfinancemanager.manager.model.dto.TransactionDto;
+import capstone.myfinancemanager.manager.service.PictureService;
 import capstone.myfinancemanager.manager.service.TransactionService;
 import capstone.myfinancemanager.manager.service.UserService;
+import com.cloudinary.Cloudinary;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -21,26 +26,30 @@ public class TransactionController {
 
     private final TransactionService transactionService;
     private final UserService userService;
+    private final Cloudinary cloudinary;
+    private final PictureService pictureService;
 
     @GetMapping
     public List<TransactionDto> getAllTransactions() {
         Principal principal = SecurityContextHolder.getContext().getAuthentication();
 
-        return transactionService.getAllTransactions(principal.getName());
+        return transactionService
+                .getAllTransactions(principal.getName());
     }
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<TransactionDto> addTransaction(@RequestBody TransactionCreationDto transactionCreation) {
+    public ResponseEntity<TransactionDto> addTransaction(@RequestPart(value = "TransactionCreationDto") TransactionCreationDto transactionCreation, @RequestParam(value = "file") Optional<MultipartFile> inputFile) throws IOException {
         Principal principal = SecurityContextHolder.getContext().getAuthentication();
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(buildNewTransactionDto(
-                                transactionService
-                                        .addTransaction(transactionCreation, principal.getName())
-                        )
-                );
+                .body(buildNewTransactionDto2(transactionService.addTransaction(transactionCreation, principal.getName()), pictureService.getFileUrl(inputFile, principal.getName())));
+    }
+
+    @PostMapping("/picture")
+    public String getUlr(@RequestPart("file") Optional<MultipartFile> inputFile) {
+        return pictureService.getFileUrl(inputFile, "");
     }
 
     @DeleteMapping("/{id}")
@@ -50,13 +59,24 @@ public class TransactionController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TransactionDto> updateTransaction(
-            @PathVariable String id,
-            @RequestBody TransactionCreationDto transactionUpdate) {
-
+    public ResponseEntity<TransactionDto> updateTransaction(@PathVariable String id,
+                                                            @RequestBody TransactionCreationDto transactionUpdate) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(buildNewTransactionDto(transactionService.updateTransaction(id, transactionUpdate)));
+    }
+
+    public TransactionDto buildNewTransactionDto2(Transaction transaction, String url) {
+        return TransactionDto.builder()
+                .id(transaction.getId())
+                .userEmail(transaction.getUserEmail())
+                .description(transaction.getDescription())
+                .amount(transaction.getAmount())
+                .transactionDate(transaction.getTransactionDate())
+                .category(transaction.getCategory())
+                .isIncome(transaction.getIsIncome())
+                .pictureId(url)
+                .build();
     }
 
     public TransactionDto buildNewTransactionDto(Transaction transaction) {
