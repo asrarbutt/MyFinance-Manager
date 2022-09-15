3e14a76b1875e6b418179580b229e6e0e7db7e15
asrarbutt <asrarbutt1986@gmail.com>
2022-09-15 14:16:47 +0200

    add Page for show alle Images

---
+++ frontend/src/components/ShowImages.tsx
@@ -0,0 +1,31 @@
+import {useContext} from "react";
+import TransactionContext from "../context/transaction/TransactionContext";
+import "./ShowImages.css"
+import ButtonStyled from "./ui/Button.styled";
+import {useNavigate} from "react-router-dom";
+
+export default function ShowImages() {
+    const {allTransactions} = useContext(TransactionContext);
+    const navigate = useNavigate();
+
+    return (
+        <div className="showImages-CardContainer">
+            <div className="showImages-header">
+                <ButtonStyled variant={"home"} onClick={() => navigate("/home")}>Home</ButtonStyled>
+                <h1>Bilder</h1>
+            </div>
+            {allTransactions.length !== 0 ? (
+                allTransactions.map(t =>
+                    <div key={t.id} className="showImages-card">
+
+                        <img src={t.pictureId} alt="No Image"/>
+
+
+                    </div>
+                )
+            ) : (<p>Keine Transaktion vorhanden</p>)
+            }
+
+        </div>
+    )
+}
