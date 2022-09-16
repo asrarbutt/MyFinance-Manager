b5d261f270bb355782823c84dd3036835053d7c7
asrarbutt <asrarbutt1986@gmail.com>
2022-09-16 10:51:40 +0200

    clean code

--- backend/src/main/java/capstone/myfinancemanager/manager/service/PictureService.java
+++ backend/src/main/java/capstone/myfinancemanager/manager/service/PictureService.java
@@ -1,8 +1,5 @@
 package capstone.myfinancemanager.manager.service;
 
-import capstone.myfinancemanager.manager.model.Picture;
-import capstone.myfinancemanager.manager.model.RandomUUIDGenerator;
-import capstone.myfinancemanager.manager.respository.PictureRepo;
 import com.cloudinary.Cloudinary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,24 +12,20 @@ import java.util.Optional;
 @Service
 public class PictureService {
 
-    private final PictureRepo pictureRepo;
-    private final Cloudinary cloudinary;
-    private final RandomUUIDGenerator randomUUIDGenerator;
 
-    public PictureService(PictureRepo pictureRepo, Cloudinary cloudinary, RandomUUIDGenerator randomUUIDGenerator) {
-        this.pictureRepo = pictureRepo;
+    private final Cloudinary cloudinary;
+
+
+    public PictureService(Cloudinary cloudinary) {
         this.cloudinary = cloudinary;
-        this.randomUUIDGenerator = randomUUIDGenerator;
     }
 
-    public String getFileUrl(Optional<MultipartFile> file, String username) {
+    public String getFileUrl(Optional<MultipartFile> file) {
         try {
             if (file.isPresent()) {
                 File fileToUpload = File.createTempFile("file", null);
                 file.get().transferTo(fileToUpload);
                 Map response = cloudinary.uploader().upload(fileToUpload, Map.of());
-                Picture picture = Picture.builder().id(randomUUIDGenerator.getRandomId()).username(username).url(response.get("url").toString()).build();
-                pictureRepo.save(picture);
                 return response.get("url").toString();
             }
 
